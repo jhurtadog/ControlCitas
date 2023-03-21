1c7fd4f3a669003fee1a237753c3a787e5356887
Jose Hurtado <57419008+jhurtadog@users.noreply.github.com>
2023-03-21 15:02:44 -0500

    ADD : Primer commint
    Base inicial del proyecto de SharePoint Citas Web Part

---
+++ src/webparts/controlCitas/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IControlCitasWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ControlCitasWebPartStrings' {
+  const strings: IControlCitasWebPartStrings;
+  export = strings;
+}
